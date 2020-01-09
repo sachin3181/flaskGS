d5303c4e4040f39aecac6e861a5b622b57a66c70
Sachin Shenoy <sachin3181@gmail.com>
2020-01-09 23:15:42 +0000

    update thru pycharm 1/10/2020

--- flashcards.py
+++ flashcards.py
@@ -13,6 +13,6 @@ def welcome():
 def card_view(index):
     try:
         card = db[index]
-        return render_template("card.html", card=card)
+        return render_template("card.html", card=card, index=index)
     except IndexError:
         abort(404)
